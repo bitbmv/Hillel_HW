86bd5a5c891fcef84377ce9b800618e2bfbb0b0d
bitbmv <bitbmv17@gmail.com>
2020-10-19 22:43:34 +0300

    29

--- HW29/src/Cache.java
+++ HW29/src/Cache.java
@@ -7,16 +7,17 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Cache<K, V> {
     private final ConcurrentHashMap<Integer, CacheObj> cacheMap = new ConcurrentHashMap<>();
 
-    public String put(K key, V value, long ttl) {
+    // сохранить в кэше значение ключа с указанием времени жизни
+    public String put(K key, V value, long ttl_milliseconds) {
         if (key == null) {
             System.err.println("'key' can't be null!");
         }
-        cacheMap.put(key.hashCode(), new CacheObj(value, ttl));
-        return "value for key='" + key.toString() + "' is posted to cache with TTL=" + ttl;
+        cacheMap.put(key.hashCode(), new CacheObj(value, ttl_milliseconds));
+        return "value for key='" + key.toString() + "' is posted to cache with TTL=" + ttl_milliseconds;
     }
 
+    // получить значение по ключу (исключения NoSuchElementException при отсутствии ключа в кэше или при истечении срока годности закешированного значения)
     public V get(K key) throws NoSuchElementException {
-
         CacheObj cacheObj;
         int hashCode = key.hashCode();
         cacheObj = cacheMap.get(hashCode);
@@ -32,11 +33,17 @@ public class Cache<K, V> {
         return cacheObj.value;
     }
 
+    // удалить из кэша значения по ключу
     public void remove(K key) {
         cacheMap.remove(key.hashCode());
     }
 
+    // очисть весь кэш
+    public void clearAll() {
+        cacheMap.clear();
+    }
 
+    // класс для хранения значения и времени до которого это значение актуально
     private class CacheObj {
         private final V value;
         private final long bestBefore;
